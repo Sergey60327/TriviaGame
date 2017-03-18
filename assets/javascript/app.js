00ca53eee2d8b2aa1beeffcd652437308af15617
dblanchard13 <davidb.prof@gmail.com>
2017-03-18 16:38:41 -0700

    Homework comments.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
+// While it may not seem imperative for smaller programs, you should get in the habit
+// linking to a separate js file and also wrapping your js code in either a 
+// $(document).ready(function(){
+//  // code goes here
+// })
+// or an IIFE (immediately invoked function expression)
+// ;(function(){
+//  // code goes here
+// })()
+// One of the most important reasons for that is security - because right now your global variables
+// can be tampered with through the console by a malicious visitor to your trivia game 😮
+
 var quizBody = $('#quiz');
 
 $(document).on('click', '#start', function(e){
@@ -40,19 +52,25 @@ var game = {
   correct: 0,
   incorrect: 0,
   counter: 20,
+  timer: null,
 
   countdown: function(){
-    game.counter--;
-    $('#counter-number').html(game.counter);
+    this.counter--;
+    $('#counter-number').html(this.counter);
 
-    if (game.counter === 0){
+    if (this.counter === 0){
       console.log('TIME UP');
-      game.done();
+      this.done();
     }
   },
 
   start: function() {
-    timer = setInterval(game.countdown, 1000);
+    // binding the context here allows you to use `this` throughout your other methods
+    // which is useful because it decouples it from the name of the containing object.
+    // You also should generally avoid setting global variables, which you were doing
+    // by not declaring timer anywhere with var. Since it is a part of the game, I
+    // would suggest just making it a property like I did above.
+    this.timer = setInterval(this.countdown.bind(this), 1000);
     $('#container').prepend('<h2>Time Remaining: <span id="counter-number">20</span> Seconds</h2>');
     $('#start').remove();
 
@@ -66,60 +84,27 @@ var game = {
   },
   
   done: function(){
+    // There's only every going to be one checked radio button, so there's no need to
+    // put this logic in a .each iterator. However, you could instead iterate over the 
+    // questions array and DRY up this code a bit. Like so:
 
-    $.each($("input[name='question-0']:checked"), function(){
-       if ($(this).val() == questions[0].correctAnswer) {
-        console.log(this);
-          game.correct++;
-      } else {
-          game.incorrect++;
+    questions.forEach(function (question, index) {
+
+      if ( $("input[name='question-" + index + "']:checked").val() === questions[index].correctAnswer ) {
+        this.correct++;
       }
-
-    });
-    $.each($("input[name='question-1']:checked"), function(){
-       if ($(this).val() == questions[1].correctAnswer) {
-          game.correct++;
-      } else {
-          game.incorrect++;
+      else {
+        this.incorrect++;
       }
 
-    });
-    $.each($("input[name='question-2']:checked"), function(){
-       if ($(this).val() == questions[2].correctAnswer) {
-          game.correct++;
-      } else {
-          game.incorrect++;
-      }
-    });
-     $.each($("input[name='question-3']:checked"), function(){
-       if ($(this).val() == questions[3].correctAnswer) {
-          game.correct++;
-      } else {
-        game.incorrect++;
-      }
-    });
-    $.each($("input[name='question-4']:checked"), function(){
-       if ($(this).val() == questions[4].correctAnswer) {
-          game.correct++;
-      } else {
-        game.incorrect++;
-      }
-    });
-   $.each($("input[name='question-5']:checked"), function(){
-       if ($(this).val() == questions[5].correctAnswer) {
-          game.correct++;
-      } else {
-        game.incorrect++;
-      }
-
-    });
+    }, this); // you need to pass the context in order for the `correct` and `incorrect` incrementers to work
 
     this.results();
   },
 
-
-     results: function(){
-      clearInterval(timer);
+  // always try to keep your indentation consistent
+  results: function(){
+    clearInterval(this.timer);
 
     $('#subwrapper h2').remove();
     quizBody.html('<h2>Results!</h2>');
@@ -127,5 +112,5 @@ var game = {
     quizBody.append('<h3>Incorrect Answers: ' + this.incorrect + '</h3>');
     quizBody.append('<h3>Unanswered: ' + (questions.length - (this.incorrect + this.correct)) + '</h3>');
 
-    }
+  }
 };
